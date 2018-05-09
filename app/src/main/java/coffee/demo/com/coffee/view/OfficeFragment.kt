8cba86a84562545f40b7f5c9897b8b70f282bfc4
nightrain <i486dx@yandex.ru>
2018-05-09 23:01:47 +0300

    Resources

--- app/src/main/java/coffee/demo/com/coffee/view/OfficeFragment.kt
+++ app/src/main/java/coffee/demo/com/coffee/view/OfficeFragment.kt
@@ -15,7 +15,7 @@ import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
 class OfficeFragment : Fragment() {
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
     }
